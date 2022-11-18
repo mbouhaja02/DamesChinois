1a6881514d66534aebc1228af9673799bc55cc4a
BOUHAJA Mohammed <Mohammed.Bouhaja@bordeaux-inp.fr>
2022-11-18 15:03:36 +0100

    neig

---
+++ src/neighbors.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "geometry.h"
+#include <limits.h> 
+#include "neighbors.h"
+
+typedef struct neighbors_t neighbor ; 
+ ; 
+
+
+void init_neighbors(unsigned int seed)
+{ 
+    if (seed < MAX_RELATIONS )
+    { 
+        neighbor g ; 
+    }
+}
+
+unsigned int get_neighbor(unsigned int idx, enum dir_t d)
+{   
+    neighbor n;
+  
+  
+  
+  
+    return 
+}
