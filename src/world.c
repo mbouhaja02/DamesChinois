8e9bb818abc63f7e4e71cffa33f996400dc3ab2e
BOUHAJA Mohammed <Mohammed.Bouhaja@bordeaux-inp.fr>
2022-11-22 15:33:47 +0100

    last

--- src/world.c
+++ src/world.c
@@ -19,21 +19,21 @@ struct world_t* world_init() {
 
 /** Return the color at a place */
 enum color_t world_get(const struct world_t* b, unsigned int idx){
-    enum color_t c = *b[idx][0];
+    enum color_t c = b->positions[idx][0];
     return c;
 }
 
 /** Sets the color at a place */
 void world_set(struct world_t* b, unsigned int idx, enum color_t c){
-    &b[idx][0] = c;
+    b->positions[idx][0] = c;
 }
 
 /** Return the sort at a place */
 enum sort_t world_get_sort(const struct world_t* b, unsigned int idx){
-    return &b[idx][1];
+    return b->positions[idx][1];
 }
 
 /** Sets the sort at a place */
 void world_set_sort(struct world_t* b, unsigned int idx, enum sort_t c){
-    &b[idx][1] = c;
+    b->positions[idx][1] = c;
 }
