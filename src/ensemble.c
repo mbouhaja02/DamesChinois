e8cad0025b48a96fda0c13d358b7a8b50968d1a2
BOUHAJA Mohammed <Mohammed.Bouhaja@bordeaux-inp.fr>
2022-11-25 15:03:14 +0100

    ensemble

--- src/ensemble.c
+++ src/ensemble.c
@@ -6,4 +6,12 @@
 #include "neighbors.h"
 #include "wolrd.h"
 
-   
+
+struct ensemble{
+    int nbr_pawn_player;
+    unsigned int positions_init[nbr_pawn_player * 2] = {0};
+    unsigned int positionw_black[nbr_pawn_player] = {0};
+    unsigned int positions_white[nbr_pawn_player] = {0};
+} ensemble;
+
+typedef struct ensemble
