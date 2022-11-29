308aea9e61507f9adc9ca76e19f56f3babeecda4
ELOUAZZANI Amira <aelouazzani@enseirb-matmeca.fr>
2022-11-29 14:57:04 +0100

    pas de pos.h

---
+++ src/victoire.h
@@ -0,0 +1,16 @@
+#ifndef __VICTOIRE_H__
+#define __VICTOIRE_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "geometry.h"
+#include "neighbors.h"
+#include "world.h"
+#include "ensemble.h"
+#include "movements.h"
+
+
+
+#endif
