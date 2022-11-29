b1b57d10a10be6313363a2b9705b73409a38ef4b
BOUHAJA Mohammed <Mohammed.Bouhaja@bordeaux-inp.fr>
2022-11-29 17:06:18 +0100

    partie

---
+++ src/partie.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "geometry.h"
+#include "neighbors.h"
+#include "world.h"
+#include "ensemble.h"
+#include "partie.h"
+
---
+++ src/partie.h
@@ -0,0 +1,17 @@
+#ifndef __PARTIE_H__
+#define __PARTIE_H__
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "geometry.h"
+#include "neighbors.h"
+#include "world.h"
+#include "ensemble.h"
+
+
+
+#endif // __PARTIE_H__
