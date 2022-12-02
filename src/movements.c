1090593157a2f67a266f1a1ac8b0bf6fc363a061
BOUHAJA Mohammed <Mohammed.Bouhaja@bordeaux-inp.fr>
2022-12-02 14:51:02 +0100

    changes

--- src/movements.c
+++ src/movements.c
@@ -15,6 +15,8 @@
 /* Fonction qui retourne l'ensemble des déplacements simple possible */
 void deplacements_simple(struct world_t* w , unsigned int idx , struct ensemble* ds ){
     unsigned int gn;
+    positions_init(ds);
+    
     for (enum dir_t j = SEAST; j < NWEST ;j++){
         gn = get_neighbor(idx , j);
         if (world_get_sort(w , gn ) == 0){
@@ -43,7 +45,6 @@ void saut_multiple(struct world_t* w , unsigned int idx , struct ensemble* sm ){
         while (place_visited ( sm, idx ) == 0){
             saut_simple(w,idx,sm);
             idx = get_neighbor(idx , j);
-            ajout_position(sm,idx);
         }
     }
     
@@ -52,10 +53,9 @@ void saut_multiple(struct world_t* w , unsigned int idx , struct ensemble* sm ){
 /* Fonction qui retourne l'ensemble des mouvements disponibles en concatenons tous les ensemble précédents */
 struct ensemble* mvts_disponibles (struct world_t* w, unsigned int idx, struct ensemble* md ) 
 {  
-    struct ensemble sm;
+    struct ensemble* sm;
     deplacements_simple( w , idx , md );
-    saut_multiple( w , idx , &sm);
-    concatener_ensembles ( md , &sm );
+    saut_multiple( w , idx , sm);
+    concatener_ensembles ( md , sm );
     return md;
 }
-
