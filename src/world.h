764d0cdecc7083171c04497b83ebf09466a343c2
BOUHAJA Mohammed <Mohammed.Bouhaja@bordeaux-inp.fr>
2023-01-03 18:01:08 +0100

    forge test

--- src/world.h
+++ src/world.h
@@ -8,7 +8,6 @@
 struct world_t{
     unsigned int positions[WORLD_SIZE][2];
 };
-
 /** Initializes a world (NO_COLOR, NO_SORT everywhere)
     Always returns the same pointer */
 struct world_t* world_init();
